5881942019c59eae2f82233c71836d69850a5733
Your Name <you@example.com>
2020-01-23 08:36:38 +0000

    up

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,5 @@ gcloud container clusters create cluster-$1 --num-nodes=3 -m g1-small
 gcloud container clusters get-credentials cluster-$1
 sh ./deployment.sh apply $1 eu.gcr.io/app1-265921/image-$1 $2 $3
 sh ./service.sh apply $1 $2
-sh ./cert.sh apply $1 gcloud.$1.cedric-bour.fr
+sh ./cert.sh apply $1 $1.gcloud.cedric-bour.fr
 sh ./ingress.sh apply $1 $2
